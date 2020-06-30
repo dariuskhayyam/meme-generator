aab7f15000bac90ef6866c873ae4504fbb8cfccf
darius khayyam <dv.Khayyam@gmail.com>
2020-06-30 13:51:39 +0200

    Updates

---
+++ precache-manifest.12240929ef6ca2919b56f5094c5286d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b057448db73138470cb2d4f4d0e72fa1",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "f43b252451e05c1b66a0",
+    "url": "/meme-generator/static/js/2.94a0e805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.94a0e805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09eaa491570c382c920a",
+    "url": "/meme-generator/static/js/main.84b87953.chunk.js"
+  },
+  {
+    "revision": "c1e33de281c57d0f508e",
+    "url": "/meme-generator/static/js/runtime-main.b3cb52fb.js"
+  }
+]);
